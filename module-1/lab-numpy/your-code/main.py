d1f604b509a928fabf94c9b5e8c3161582971219
LuisFer <luisferlc1515@gmail.com>
2019-10-30 16:12:17 -0600

    solución lab-numpy

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,67 +1,73 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+np.version.version
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.full((5,3,2),1)
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+print(a.size == b.size)
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#No, porque no tienen la misma forma
+a+b
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = b.T
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+#Ahora son de la misma forma (2,3,5)
+d = a+c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+#Dos arreglos que tienen la misma forma
+a
+d
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a*c
 
 #13. Does e equal to a? Why or why not?
 
-
+#Si, son iguales porque se mutiplicaron por 1
+e==a
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = d.max()
+d_min = d.min()
+d_mean = d.mean()
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty((2,3,5))
 
 
 
@@ -74,10 +80,20 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+#https://www.pluralsight.com/guides/numpy-arrays-iterating
 
-
-
-
+for j,e in np.nditer([f,d], op_flags=['readwrite']):
+    if e > d_min and e < d_mean:
+        j[...] = 25
+    elif e > d_mean and e < d_max:
+        j[...] = 75
+    elif e == d_mean:
+        j[...] = 50
+    elif e == d_min:
+        j[...] = 0
+    elif e == d_max:
+        j[...] = 100
+            
 """
 #17. Print d and f. Do you have your expected f?
 For instance, if your d is:
@@ -99,6 +115,9 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
 
+d
+f
+
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +131,17 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+#g = np.empty((2,3,5))
+#g = list(g)
+#
+#for j,e in np.nditer([g,d], op_flags=['readwrite']):
+#    if e > d_min and e < d_mean:
+#        j[...] = 'B'
+#    elif e > d_mean and e < d_max:
+#        j[...] = 'D'
+#    elif e == d_mean:
+#        j[...] = 'C'
+#    elif e == d_min:
+#        j[...] ='A'
+#    elif e == d_max:
+#        j[...] = 'E'
